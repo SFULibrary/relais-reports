131a787b5893ca60d1a9890c5671461f333c5883
Michael Joyce <ubermichael@gmail.com>
2014-08-22 13:13:38 -0700

    ui improvements 

--- htdocs/js/relais.js
+++ htdocs/js/relais.js
@@ -1,3 +1,11 @@
+
 $(document).ready(function(){
 	$("table.sortable").tablesorter();
+	$("td.requestnum").each(function(){
+		var $td = $(this);
+		var txt = $td.html();
+		var $input = $("<input readonly='readonly' value='" + txt + "'/>");
+		$td.empty().append($input);
+		$input.click(function(){this.select()});
+	});
 });
